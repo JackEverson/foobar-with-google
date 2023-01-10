92e5d5e38bea6755b428ccf581c636e9d73cb6f6
Jack Everson <jack.everson@outlook.com>
2023-01-10 19:50:54 +1000

    finished fuel-injection-perfection

--- 03fuel-injection-perfection/solution.py
+++ 03fuel-injection-perfection/solution.py
@@ -1,28 +1,19 @@
 def solution(n):
     nint = int(n)
-    print("starting value is:" + str(n))
     movecount = 0 #counter for how many actions have been made
 
-    while n != 1:
-        if n%2 == 0:        #dividing by a half is always the greatest preference (if possible)
-            n=n/2
+    while nint != 1:
+        if nint%2 == 0:        #dividing by a half is always the greatest preference (if possible)
+            nint=nint/2
             movecount += 1
-            print("n: "+str(n)+"     movecount: "+str(movecount))
 
-        elif ((n-1)/2)%2 != 0:
-            n+=1
+        elif ((nint+1)%4)==0 and nint!=3:
+            nint+=1
             movecount+=1
-            print("n: "+str(n)+"     movecount: "+str(movecount))
-        
+           
         else:
-            n-=1
+            nint-=1
             movecount+=1
-            print("n: "+str(n)+"     movecount: "+str(movecount))
-
+           
     return movecount
 
-
-#testing area
-solution(15)
-
-solution(4)
